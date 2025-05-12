056d200f1b6e66fedd99a18bb6113c83f00b6b30
gokula <gokulakrishnanr812@gmail.com>
2025-05-12 16:17:07 +0530

    Initial Commit

---
+++ kerlig/kerligApp.swift
@@ -0,0 +1,17 @@
+//
+//  kerligApp.swift
+//  kerlig
+//
+//  Created by gokul on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct kerligApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kerligTests/kerligTests.swift
@@ -0,0 +1,17 @@
+//
+//  kerligTests.swift
+//  kerligTests
+//
+//  Created by gokul on 12/05/25.
+//
+
+import Testing
+@testable import kerlig
+
+struct kerligTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
